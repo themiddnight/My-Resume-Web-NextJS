d27a855899cabeb9e13b78c3a3a61d272918836c
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-08-21 09:20:08 +0700

    chore: Add EditLayout and EditPage components for create/[id] and create/login pages

---
+++ src/app/create/[id]/layout.js
@@ -0,0 +1,9 @@
+export default function EditLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/create/login/page.js
@@ -0,0 +1,8 @@
+export default function EditPage() {
+  return (
+    <main>
+      <h1>Login Page</h1>
+      <a href="/">Back home</a>
+    </main>
+  );
+}
